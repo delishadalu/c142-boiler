2b176594d23d6c90b9ee3418de76b191ff5c9f45
delishadalu <73183936+delishadalu@users.noreply.github.com>
2022-11-18 12:30:21 +0530

    Add files via upload

---
+++ navigation/stack.js
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+import Home from "../screen/home"
+import TopNavigator from "./toptab"
+
+const Stack = createStackNavigator();
+
+import { createStackNavigator } from '@react-navigation/stack';
+
+
+export default class App extends React.Component {
+  render() {
+    return(
+    
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="Top" component={TopNavigator} />
+          <Stack.Screen name="Home" component={Home} />
+        </Stack.Navigator>
+     
+    );
+  }
+}
